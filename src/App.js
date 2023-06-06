c41ee3f7f093a463752bee600a8d112bf3afabb3
anyajegorova <anna.jerm@outlook.com>
2023-06-06 17:12:33 +0300

    added images and some styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css'
+import Gallery from './Gallery';
 
 function App() {
   return (
     <>
-    <h1 style={{textAlign: 'center'}}>My cats image gallery</h1>
+      <h1 style={{ textAlign: 'center' }}>My cats image gallery</h1>
+      <Gallery />
     </>
   );
 }
---
+++ src/Gallery.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import './gallery.css';
+import Img1 from './images/1.JPG';
+import Img2 from './images/2.JPG';
+import Img3 from './images/3.JPG';
+import Img4 from './images/4.JPG';
+import Img5 from './images/5.JPG';
+import Img6 from './images/6.JPG';
+import Img7 from './images/7.JPG';
+import Img8 from './images/8.JPG';
+import Img9 from './images/8.JPG';
+import Img10 from './images/10.JPG';
+
+const Gallery = () => {
+    const data = [
+        {
+            id: 1,
+            imgSrc: Img1
+        },
+        {
+            id: 2,
+            imgSrc: Img2
+        },
+        {
+            id: 3,
+            imgSrc: Img3
+        },
+        {
+            id: 4,
+            imgSrc: Img4
+        },
+        {
+            id: 5,
+            imgSrc: Img5
+        },
+        {
+            id: 6,
+            imgSrc: Img6
+        },
+        {
+            id: 7,
+            imgSrc: Img7
+        },
+        {
+            id: 8,
+            imgSrc: Img8
+        },
+        {
+            id: 9,
+            imgSrc: Img9
+        },
+        {
+            id: 10,
+            imgSrc: Img10
+        },
+    ]
+    return (
+        <>
+            <div className='gallery'>
+                {data.map((item, index) => {
+                    return (
+                        <div className='images' key={index}>
+                            <img src={item.imgSrc} style={{width: '100%'}}/>
+                        </div>
+                    )
+                })}
+
+            </div>
+        </>
+
+    )
+}
+
+export default Gallery;
